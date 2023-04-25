da3aa5c1d384f608a27ed451a08efd8d9b822acd
zowie <zowietao@gmail.com>
2023-04-25 14:49:32 +0800

    chore: add env file and some module

---
+++ environment.d.ts
@@ -0,0 +1,10 @@
+namespace NodeJS {
+    interface ProcessEnv {
+        readonly NEXT_PUBLIC_APP_ENV:string
+        readonly NEXT_PUBLIC_API_HOST:string
+        readonly NEXT_PUBLIC_APP_URI:string
+        readonly NEXT_PUBLIC_APP_ASSETS:string
+        readonly NEXT_PUBLIC_PLUGIN_ID:string
+    }
+}
+
--- next.config.js
+++ next.config.js
@@ -1,12 +1,32 @@
 /** @type {import('next').NextConfig} */
 
+const dotenv = require('dotenv');
+
+const loadEnv = () => {
+  const env = {
+    NEXT_PUBLIC_APP_ENV: process.env.APP_ENV || process.env.NODE_ENV,
+  };
+  const loaded = dotenv.config({
+    path: `.env.${env.NEXT_PUBLIC_APP_ENV}`,
+    silent: true,
+  });
+  Object.keys(process.env).forEach((key) => {
+    if (key.startsWith('NEXT_PUBLIC_')) {
+      env[key] = loaded.parsed[key];
+    }
+  });
+
+  return env;
+};
+
 const nextConfig = {
+  env: loadEnv(),
   reactStrictMode: true,
   webpack: function (config, options) {
     config.resolve.fallback = {
       crypto: false,
       path: false,
-      fs: false
+      fs: false,
     };
     config.module.rules.push({
       test: /\.(png|jpg|gif|eot|ttf|woff|woff2)$/i,
@@ -17,15 +37,16 @@ const nextConfig = {
           name: '[path][name].[ext]',
           encoding: 'base64',
           fallback: require.resolve('file-loader'),
-        }
-      }
-    })
+        },
+      },
+    });
     config.module.rules.push({
       test: /\.svg$/i,
       issuer: /\.[jt]sx?$/,
       use: ['@svgr/webpack'],
-    },)
-    return config
+    });
+
+    return config;
   },
   images: {
     disableStaticImages: true,
